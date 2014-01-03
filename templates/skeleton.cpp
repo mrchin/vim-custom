c95879656794b5f86bf8f9e54e3b24e9ed66a8a0
Kevin Manalo <kmanalo@gmail.com>
2014-01-03 14:36:03 -0600

    First comit

---
+++ templates/skeleton.cpp
@@ -0,0 +1,15 @@
+/**********************************************************
+ * File:FILENAME
+ * 
+ * Desc: 
+ *
+ * Date:DATE 
+ *
+ * Author: Kevin Manalo
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+     
+}
